0aa8ef9e2408b559ac76ff64add05a89315bb228
Oliveira Da Silva <jaoliveira@indra.es>
2018-09-04 13:21:40 -0300

    Add component base to learn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { MenuComponentComponent } from './menu-component/menu-component.component';
@@ -10,7 +11,8 @@ import { MenuComponentComponent } from './menu-component/menu-component.componen
     MenuComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
