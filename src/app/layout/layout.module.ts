1e53125d02decd1057f1147d619429df7cf52c72
Oliveira Da Silva <jaoliveira@indra.es>
2018-09-05 15:37:47 -0300

    Add components layout and css

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -11,9 +13,9 @@ import { HeadComponent } from './head/head.component';
     CommonModule,
     NgbModule.forRoot(),
   ],
-  declarations: [HeadComponent],
-  exports:[
-    HeadComponent
+  declarations: [HeadComponent, BodyComponent, FooterComponent],
+  exports: [
+    HeadComponent, BodyComponent, FooterComponent
   ]
 })
 export class LayoutModule { }
