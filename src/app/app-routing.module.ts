150925fc4ed6ecb82db2a943b7ea383233a44385
Oliveira Da Silva <jaoliveira@indra.es>
2018-09-07 10:38:21 -0300

    Add modules to get lazy load of components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './layout/body/body.component';
 
 const routes: Routes = [
-  {
-    path:'', component: BodyComponent
-  }
+  { path: '', component: BodyComponent },
+  { path: 'about', loadChildren: './about/about.module#AboutModule' },
+  { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
+  { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule' }
 ];
 
 @NgModule({
