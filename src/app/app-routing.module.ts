de082f8899dca3fe2ca4635cd37d609aed8b1e2f
Oliveira Da Silva <jaoliveira@indra.es>
2018-09-10 08:31:53 -0300

    add file modularize to layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './layout/body/body.component';
 
 const routes: Routes = [
-  { path: '', component: BodyComponent },
   { path: 'about', loadChildren: './about/about.module#AboutModule' },
   { path: 'contact', loadChildren: './contact/contact.module#ContactModule' },
   { path: 'projects', loadChildren: './projects/projects.module#ProjectsModule' }
