36363228b420784685184e67b7c50ac87fed9541
Josemalyson Oliveira <josemalyson.silva>
2019-07-20 05:04:19 -0300

    Modificando about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   apresentation = {
-    tittle: "My live",
+    tittle: "Josemalyson",
     content: "My professional life began when I was recruited to the army in 2008 and there I was able to know the area of technology starting in the area of linux server networks. In 2010 I started my degree in Computer Science at Unipê. In 2012 I participated in the software factory composed of students from the IT courses from there I went to the internship in the CREA-PB federal body of the Brazilian government being assigned to the technology area of the same. So in 2012 I was selected for the Phoebus Technology Company to be technical support working with Oracle PL / SQL and SqlServer. Then started my search for the development area where I had the first opportunity in Indra Brazil when I started my career as a developer in 2015, so I grew professionally and was invited to join the software development team of Stefanini in 2017. Today I always look for the better for my family trying to be the best and most committed professional in the company."
   };
 
   constructor() {
-   
+
   }
 
   ngOnInit() {
-    
+
   }
 
 }
